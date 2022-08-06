0c14e5edf3df37ecde30c60fed06797510633eca
Yuri Mel0 <meloy316@gmail.com>
2022-08-06 20:26:03 -0300

    Finalizando o exemplo com forms em HTML integrando formulario com API express
     Mudanças feitas nesse commit:
    	modified:   exercicios/form/form.html
    	modified:   exercicios/form/package-lock.json
    	modified:   exercicios/form/package.json
    	modified:   exercicios/form/server.js

--- exercicios/form/server.js
+++ exercicios/form/server.js
@@ -7,7 +7,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/usuarios', (req, res) => {
     console.log(req.body)
-    res.send('<h1>Parabéns</h1>')
+    res.send('<h1>Parabéns, Usuario Incluido</h1>')
+})
+
+app.post('/usuarios/:id', (req, res) => {
+    console.log(req.body.id)
+    console.log(req.body)
+    res.send('<h1>Parabéns, Usuario Alterado</h1>')
 })
 
 app.listen(port)

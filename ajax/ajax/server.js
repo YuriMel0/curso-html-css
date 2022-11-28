4864eab8b85eec0883d23b5c99d3d175599587ae
YuriMel0 <yurimelo216@gmail.com>
2022-11-28 20:13:39 -0300

    adicionando segundo exemplo com XMLHttpRequest e fazendo upload de arquivos

--- ajax/ajax/server.js
+++ ajax/ajax/server.js
@@ -6,4 +6,27 @@ app.use(express.static('.'))
 app.use(express.urlencoded({ extended: true}))
 app.use(badyParser.json())
 
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, '.')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err) {
+            return res.send('Ocorreu um erro...')
+        }
+
+        res.send('Concluido com sucesso')
+    })
+})
+
 app.listen(8080, () => console.log('http://localhost:8080'))

f7b6045bf0849dc53ebada87ab086f2a2d188242
YuriMel0 <yurimelo216@gmail.com>
2022-11-28 15:59:26 -0300

    organizando ambiente do projeto

--- ajax/ajax/server.js
+++ ajax/ajax/server.js
@@ -6,8 +6,4 @@ app.use(express.static('.'))
 app.use(express.urlencoded({ extended: true}))
 app.use(badyParser.json())
 
-app.get('/teste', (req, res) => {
-    res.send('OK')
-})
-
 app.listen(8080, () => console.log('http://localhost:8080'))
